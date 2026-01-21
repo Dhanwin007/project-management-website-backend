f690369feb8047ee7e69a4659e12e08b0154d4bd
Dhanwin007 <dhanwing05@gmail.com>
2026-01-21 22:12:40 +0530

    task routes changed with multer

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,35 +1,62 @@
 import { Router } from 'express';
 import {
-    getTasks,
-    createTask,
-    getTaskById,
-    updateTask,
-    deleteTask,
-    createSubTask,
-    updateSubTask,
-    deleteSubTask
-} from "../controllers/task.controllers.js";
-import { validate } from "../middlewares/validator.middleware.js";
-import { createTaskValidator, createSubTaskValidator } from "../validators/index.js";
-import { verifyJWT, validateProjectPermission } from "../middlewares/auth.middleware.js";
-import { UserRolesEnum } from "../utils/constants.js";
-import { upload } from "../middlewares/multer.middleware.js"; // 1. IMPORT MULTER
-
+  getTasks,
+  createTask,
+  getTaskById,
+  updateTask,
+  deleteTask,
+  createSubTask,
+  updateSubTask,
+  deleteSubTask,
+} from '../controllers/task.controllers.js';
+import { validate } from '../middlewares/validator.middleware.js';
+import {
+  createTaskValidator,
+  createSubTaskValidator,
+} from '../validators/index.js';
+import {
+  verifyJWT,
+  validateProjectPermission,
+} from '../middlewares/auth.middleware.js';
+import { UserRolesEnum } from '../utils/constants.js';
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 router.use(verifyJWT);
 
 router
-    .route("/:projectId")
-    .get(validateProjectPermission(), getTasks)
-    .post(upload.array("attachments", 5),validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]), createTaskValidator(), validate, createTask);
+  .route('/:projectId')
+  .get(validateProjectPermission(), getTasks)
+  .post(upload.array("attachments", 5),
+    validateProjectPermission([
+      UserRolesEnum.ADMIN,
+      UserRolesEnum.PROJECT_ADMIN,
+    ]),
+    createTaskValidator(),
+    validate,
+    createTask,
+  );
 
 router
-    .route("/:projectId/t/:taskId")
-    .get(validateProjectPermission(), getTaskById)
-    .put(upload.array("attachments", 5),validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]), createTaskValidator(), validate, updateTask)
-    .delete(validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]), deleteTask);
+  .route('/:projectId/t/:taskId')
+  .get(validateProjectPermission(), getTaskById)
+  .put(upload.array("attachments", 5),
+    validateProjectPermission([
+      UserRolesEnum.ADMIN,
+      UserRolesEnum.PROJECT_ADMIN,
+    ]),
+    createTaskValidator(),
+    validate,
+    updateTask,
+  )
+  .delete(
+    validateProjectPermission([
+      UserRolesEnum.ADMIN,
+      UserRolesEnum.PROJECT_ADMIN,
+    ]),
+    deleteTask,
+  );
 
 router
   .route('/:projectId/t/:taskId/subtasks')
@@ -45,11 +72,17 @@ router
   );
 
 router
-    .route("/:projectId/st/:subTaskId")
-    .put(validateProjectPermission([ // 4. ADDED ROLES (Standard users shouldn't edit subtasks usually)
+  .route('/:projectId/st/:subTaskId')
+  .put(validateProjectPermission([ // 4. ADDED ROLES (Standard users shouldn't edit subtasks usually)
       UserRolesEnum.ADMIN, 
       UserRolesEnum.PROJECT_ADMIN 
     ]), updateSubTask)
-    .delete(validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]), deleteSubTask);
+  .delete(
+    validateProjectPermission([
+      UserRolesEnum.ADMIN,
+      UserRolesEnum.PROJECT_ADMIN,
+    ]),
+    deleteSubTask,
+  );
 
 export default router;
